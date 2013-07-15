5b5d3fa8fa455a61430339ddcde317c34ab28707
J.T. Halbert <b3d3vil3d@gmail.com>
2013-07-15 10:26:28 -0400

    working skeletal parser.

--- cd-fit.hs
+++ cd-fit.hs
@@ -1,6 +1,26 @@
 module Main where
 
+import           Text.ParserCombinators.Parsec
+
+parseInput =
+    do dirs <- many dirAndSize
+       eof :: Parser ()
+       return dirs
+
+data Dir = Dir Int String deriving Show
+
+dirAndSize =
+    do size <- many1 digit
+       spaces
+       dir_name <- anyChar `manyTill` newline
+       return (Dir (read size) dir_name)
+
 main :: IO()
 main = do input <- getContents
           putStrLn ("DEBUG: got input " ++ input)
+          let dirs = case parse parseInput "stdin" input of
+                     Left err -> error $ "Input:\n" ++ show input ++
+                                         "\nError:\n" ++ show err
+                     Right result -> result
+          putStrLn "DEBUG: parsed:"; print dirs
 
